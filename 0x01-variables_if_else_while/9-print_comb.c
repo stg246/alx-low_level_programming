e2b79bc2fef7753cd760232523d7398024b36732
stg246 <113631335+stg246@users.noreply.github.com>
2022-10-15 09:43:55 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,17 @@ int main(void)
 {
 	int num;
 
-	for (1 = 0; 1 <= 9; 1++)
+	for (num = 0; num <= 9; num++)
 	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
 
 		putchar(',');
 		putchar(' ');
 	}
 
 	putchar('\n');
+
 	return (0);
 }
