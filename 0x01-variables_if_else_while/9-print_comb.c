10b6cc016a06e0c907f5ab62924499de2e4b47a4
Bright Aggrey <stg246activation@gmail.com>
2022-10-15 09:44:55 +0000

    Tasks

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,12 @@ int main(void)
 {
 	int num;
 
-	for (1 = 0; 1 <= 9; 1++)
+	for (num = 0; num <= 9; num++)
 	{
-
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
 		putchar(',');
 		putchar(' ');
 	}
@@ -19,3 +22,45 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
