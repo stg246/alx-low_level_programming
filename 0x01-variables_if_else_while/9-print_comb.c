c549504623e237aeee5301d70295bcd360d0dcc3
Bright Aggrey <stg246activation@gmail.com>
2022-10-15 06:53:35 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (1 = 0; 1 <= 9; 1++)
+	{
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
