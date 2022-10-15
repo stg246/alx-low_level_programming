7b584a54847f1768507df33d325d379a30868a03
Bright Aggrey <stg246activation@gmail.com>
2022-10-15 06:29:42 +0000

    Tasks

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,29 +1,21 @@
-/*
- * File: 9-print_comb.c
- * Auth: Bright Aggrey
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
- *
+ * main - prints all combination of single digits
  * Return: Always 0.
  */
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
+	for (n - 48; n < 58; n++)
 		
-		putchar(',');
-		putchar(' ');
-	}
-
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	putchar('\n');
 
 	return (0);
