4144daa2463c39c724612c38d0e4930ffcaaab6f
Bright Aggrey <stg246activation@gmail.com>
2022-10-18 19:43:37 +0000

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * r = row, c = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
+ */
+void times_table(void)
+{
+	int rone, cone, d;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++)
+		{
+			d = (r * c);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+}
