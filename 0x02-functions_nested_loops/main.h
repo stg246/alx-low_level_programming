756833621802420a3b4adb40d412c357a4ad5341
Bright Aggrey <stg246activation@gmail.com>
2022-10-18 17:28:35 +0000

    My first commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H
+# define MAIN_H
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -9,7 +9,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times-table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
