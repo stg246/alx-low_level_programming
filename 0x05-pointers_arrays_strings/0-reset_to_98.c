ebd4685b6673a695e084bab62c334372d32b1781
Bright Aggrey <stg246activation@gmail.com>
2022-10-24 17:43:47 +0000

    My First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to 98
+ * @n: a pointer
+ * Return : void, Basically nothing is returned
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
