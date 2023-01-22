7a04b817fa18a3efe9b3964b858f9f63df96dc0c
BRIGHT AGGREY <113631335+stg246@users.noreply.github.com>
2023-01-22 09:33:42 +0000

    Updated

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-# Create an empty array to store all the .c files in the current directory
-files=()
-
-# Find all the .c files in the current directory and add them to the array
-for file in $(ls *.c); do
-    files+=("$file")
-done
-
-# Compile all the .c files into a dynamic library called liball.so
-gcc -shared -o liball.so ${files[@]}
-
-echo "Dynamic library liball.so created successfully."
+gcc *.c -fPIC -shared -o liball.so
