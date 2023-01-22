8d66ebedfdcb408d76e023434095b029210d251d
BRIGHT AGGREY <113631335+stg246@users.noreply.github.com>
2023-01-22 09:49:31 +0000

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-mkdir ..liball.so.d
-cd ..liball.so.d
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c -fPIC ../*.c
-cd ..
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -shared ..liball.so.d/*.o -o liball.so
-rm -R ..liball.so.d
+gcc -fPIC -c *.c
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o
