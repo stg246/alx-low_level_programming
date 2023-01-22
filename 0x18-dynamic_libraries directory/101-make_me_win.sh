c2cb16b05d066293d612d0ad1e1970c3e6a40dd0
BRIGHT AGGREY <113631335+stg246@users.noreply.github.com>
2023-01-22 09:03:41 +0000

    new files

--- 0x18-dynamic_libraries directory/101-make_me_win.sh
+++ 0x18-dynamic_libraries directory/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Aysuarex/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P .. https://raw.githubusercontent.com/VicvekSr9485/alx-low_level_programming/main/0x18-dynamic_libraries/randnum.so
+export LD_PRELOAD="$PWD/../randnum.so"
