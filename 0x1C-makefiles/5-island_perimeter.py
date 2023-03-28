d4167c9e1ce6ab554266a281b380d929f1b3f1a2
BRIGHT AGGREY <113631335+stg246@users.noreply.github.com>
2023-03-28 21:06:39 +0000

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,68 @@
 #!/usr/bin/python3
-""" Island Perimeter """
 
+Args:
+grid (list[list[int]]): A rectangular grid with integers representing land and water zones.
 
-def island_perimeter(grid):
-    """ Island Perimeter """
-    peri = 0
-    gl = []
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            gl.append(grid[i][j])
-    for i in range(len(gl)):
-        if i < len(gl) - 1 and i > 0:
-            if gl[i] == 0 and gl[i + 1] == 1 or gl[i] == 0 and gl[i - 1] == 1:
-                peri += 1
-    if peri % 2 == 0:
-        peri *= 2
-    else:
-        peri = (peri * 2) - 1
-    return peri
+Returns:
+int: The perimeter of the island.
+
+Raises:
+ValueError: If the grid is invalid or contains more than one island.
+
+"""
+rows = len(grid)
+cols = len(grid[0])
+
+# check if the grid is valid
+if rows < 1 or rows > 100 or cols < 1 or cols > 100:
+    raise ValueError("Invalid grid size")
+
+# find the first land cell
+found_land = False
+for i in range(rows):
+    for j in range(cols):
+        if grid[i][j] == 1:
+            found_land = True
+            start_cell = (i, j)
+            break
+    if found_land:
+        break
+
+# check if there is more than one island
+visited = set()
+queue = [start_cell]
+while queue:
+    curr_cell = queue.pop(0)
+    if curr_cell in visited:
+        continue
+    visited.add(curr_cell)
+    i, j = curr_cell
+    if grid[i][j] == 0:
+        continue
+    if i > 0:
+        queue.append((i-1, j))
+    if i < rows-1:
+        queue.append((i+1, j))
+    if j > 0:
+        queue.append((i, j-1))
+    if j < cols-1:
+        queue.append((i, j+1))
+
+if len(visited) != sum(row.count(1) for row in grid):
+    raise ValueError("More than one island")
+
+# count the perimeter of the island
+perimeter = 0
+for i in range(rows):
+    for j in range(cols):
+        if grid[i][j] == 1:
+            if i == 0 or grid[i-1][j] == 0:
+                perimeter += 1
+            if i == rows-1 or grid[i+1][j] == 0:
+                perimeter += 1
+            if j == 0 or grid[i][j-1] == 0:
+                perimeter += 1
+            if j == cols-1 or grid[i][j+1] == 0:
+                perimeter += 1
+
+return perimeter
